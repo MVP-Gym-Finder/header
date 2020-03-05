6538228bc79e254eb6d88d5b2d8c872cf7b16a7d
Adam <gienappa@gmail.com>
2020-03-05 15:48:25 -0800

    header initial build complete

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,16 +6,41 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  loginHandler(e) {
+    e.preventDefault();
+
   }
 
   render() {
     return (
       <div className="header-container">
-        <div className="header-title">GymFinder</div>
+        <div className="header-title">
+          <h2>GymFinder</h2>
+        </div>
         <div className="header-nav">
           <div className="header-home">Home</div>
-          <div className="header-login">Login</div>
+          {this.state.isLoggedIn ? 
+            <div className="header-account">Account</div> :
+            <div className="header-login">Account
+              <div className="login-panel">
+                <h4>Login</h4>
+                <label className="login-username">Username:
+                  <br></br>
+                  <input type="text" name="username"></input>
+                </label>
+                <label className="login-pswd">Password:
+                  <br></br>
+                  <input type="password" name="password"></input>
+                </label>
+                <button>Login</button>
+              </div>
+            </div>
+          }
         </div>
       </div>
     );
